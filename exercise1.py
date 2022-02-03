1dbd8718094afd1542693cbd14d63063cd863a63
Dale Stephenson <dalestephenson@zohomail.com>
2022-02-03 21:56:06 +1300

    Exercises

--- exercise1.py
+++ exercise1.py
@@ -1,3 +1,12 @@
 print "Hi"
 
-print "yo"
+print "Hi " + "Dale"
+
+#SyntaxError: EOL while scanning a string literal
+
+#print "Mismatched quotes will cause a SyntaxError'
+#print Without quotes will cause a NameError
+
+print("How do you make a hot dog stand?")
+print('You take away its chair!')
+
