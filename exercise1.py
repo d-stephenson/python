d9a0a18933329af996425934c894127d6a276035
Dale Stephenson <dalestephenson@zohomail.com>
2022-02-03 22:02:16 +1300

    Exercises

--- exercise1.py
+++ exercise1.py
@@ -10,3 +10,16 @@ print "Hi " + "Dale"
 print("How do you make a hot dog stand?")
 print('You take away its chair!')
 
+#Create a variable 
+
+greeting_message = "Welcome to Codecademy!"
+current_excercise = 5
+
+#Python also offers a companion to division called the modulo operator. The modulo operator is indicated by % and returns the remainder after division is performed
+
+is_this_number_odd = 15 % 2
+is_this_number_divisible_by_seven = 133 % 7
+
+#In the above code block, we use the modulo operator to find the remainder of 15 divided by 2. Since 15 is an odd number the remainder is 1
+
+
