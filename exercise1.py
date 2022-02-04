6650463ae8482e957e3eba0fb783f1625b97893c
Dale Stephenson <dalestephenson@zohomail.com>
2022-02-04 17:43:16 +1300

    Exercises

--- exercise1.py
+++ exercise1.py
@@ -232,3 +232,9 @@ print str(pi)
 #--------------------------------
 
 #Dot Notation
+# Methods that use dot notation only work with strings
+# On the other hand, len() and str() can work on other data types
+
+ministry = "The Ministry of Silly Walks"
+print len(ministry)
+print ministry.upper()
