f23ca4941b92c0a8d8fa4efd8056e15ac1152291
Dale Stephenson <dalestephenson@zohomail.com>
2022-02-04 17:22:01 +1300

    Exercises

--- exercise1.py
+++ exercise1.py
@@ -191,3 +191,13 @@ int_addition = int(number1) + int(number2)
 print string_addition
 print int_addition 
 
+#--------------------------------
+
+#The amount of points for each exercise may change, because points don't exist yet
+skill_completed = "Python Syntax"
+exercises_completed = 13
+points_per_exercise = 5
+point_total = 100 + (exercises_completed * points_per_exercise)
+
+print "I got " + str(point_total) + " points!"
+
