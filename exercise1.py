6e2104f222df25686c3570e3c95ed385ee124895
Dale Stephenson <dalestephenson@zohomail.com>
2022-02-11 12:06:28 +1300

    exercises

--- exercise1.py
+++ exercise1.py
@@ -514,3 +514,22 @@ if len(original) > 0 and original.isalpha():
 else:
     print 'empty'
 
+#--------------------------------
+
+# Functions
+
+def tax(bill):
+      """Adds 8% tax to a restaurant bill."""
+  bill *= 1.08
+  print "With tax: %f" % bill
+  return bill
+
+def tip(bill):
+  """Adds 15% tip to a restaurant bill."""
+  bill *= 1.15
+  print "With tip: %f" % bill
+  return bill
+  
+meal_cost = 100
+meal_with_tax = tax(meal_cost)
+meal_with_tip = tip(meal_with_tax)
