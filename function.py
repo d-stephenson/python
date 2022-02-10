fa2a59daa3da0391d2b3a28971ae5e8b50595181
Dale Stephenson <dalestephenson@zohomail.com>
2022-02-11 12:24:54 +1300

    functions

--- function.py
+++ function.py
@@ -15,23 +15,39 @@ def tip(bill):
 meal_cost = 100
 meal_with_tax = tax(meal_cost)
 meal_with_tip = tip(meal_with_tax)
-  
+
+# Define your spam function starting on line 5. You
+# can leave the code on line 10 alone for now--we'll
+# explain it soon!
+
+def spam():
+#   """Prints 'Comment' to the console"""
+  print "Eggs!"
+
+spam()
+
+
+
+
+
+
+
+
+
 #--------------------------------
     # Functions are defined with three components:
 
     # The header, which includes the def keyword, the name of the function, and any parameters the function requires. Here’s an example:
-
     # def hello_world(): # There are no parameters
+    
     # An optional comment that explains what the function does.
-
     # """Prints 'Hello World!' to the console."""
+    
     # The body, which describes the procedures the function carries out. The body is indented, just like conditional statements.
-
     # print "Hello World!"
+    
     # Here’s the full function pieced together:
-
     # def hello_world():
     #   """Prints 'Hello World!' to the console."""
     #   print "Hello World!"
 #--------------------------------
-    
