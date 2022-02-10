ef9d765187d3009500c759e807d38fc165fcf1ed
Dale Stephenson <dalestephenson@zohomail.com>
2022-02-11 12:29:14 +1300

    functions

--- function.py
+++ function.py
@@ -26,7 +26,18 @@ def spam():
 
 spam()
 
+#--------------------------------
 
+def square(n):
+#   """Returns the square of a number."""
+  squared = n ** 2
+  print "%d squared is %d." % (n, squared)
+  return squared
+  
+# Call the square function on line 10! Make sure to
+# include the number 10 between the parentheses.
+
+square(10)
 
 
 
@@ -50,4 +61,17 @@ spam()
     # def hello_world():
     #   """Prints 'Hello World!' to the console."""
     #   print "Hello World!"
+#--------------------------------
+
+#--------------------------------
+    # def square(n):
+    #     Here, n is a parameter of square. A parameter is a variable that is an input to a function. It says, “Later, when square is used, you’ll be able to input any value you want, but for now we’ll call that future value n.” A function can have any number of parameters.
+
+    # The values of the parameters passed into a function are known as the arguments. Recall in the previous example, we called:
+
+    # py square(10)
+
+    # Here, the function square was called with the parameter n set to the argument 10.
+
+    # Typically, when you call a function, you should pass in the same number of arguments as there are parameters.
 #--------------------------------
