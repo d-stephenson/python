09bbec548a7f4bc72eb2ab3fddf21b05ae926f3c
Dale Stephenson <dalestephenson@zohomail.com>
2022-02-11 13:18:07 +1300

    functions

--- function.py
+++ function.py
@@ -71,8 +71,23 @@ def by_three(number):
   else:
     return False
 
+print cube(23)
+print by_three(23)
 
+#--------------------------------
 
+#Function Imports
+
+import math 
+print math.sqrt(25)
+
+# Import *just* the sqrt function from math on line 3!
+
+from math import sqrt
+
+# Universal import can handle this for you. The syntax for this is:
+
+from math import *
 
 #--------------------------------
     # Functions are defined with three components:
@@ -103,4 +118,16 @@ def by_three(number):
     # Here, the function square was called with the parameter n set to the argument 10.
 
     # Typically, when you call a function, you should pass in the same number of arguments as there are parameters.
+#--------------------------------
+
+#--------------------------------
+    # Careful with function modules
+
+    # Universal imports may look great on the surface, but they’re not a good idea for one very important reason: they fill your program with a ton of variable and function names without the safety of those names still being associated with the module(s) they came from.
+
+    # If you have a function of your very own named sqrt and you import math, your function is safe: there is your sqrt and there is math.sqrt. If you do from math import *, however, you have a problem: namely, two different functions with the exact same name.
+
+    # Even if your own definitions don’t directly conflict with names from imported modules, if you import * from several modules at once, you won’t be able to figure out which variable or function came from where.
+
+    # For these reasons, it’s best to stick with either import module and type module.name or just import specific variables and functions from various modules as needed.
 #--------------------------------
