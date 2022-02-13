9eb62333679098eafb7c8d5d71209ee09cc83a06
Dale Stephenson <dalestephenson@zohomail.com>
2022-02-13 20:50:24 +1300

    Loops

--- loops.py
+++ loops.py
@@ -33,3 +33,11 @@ print square_list
 # A dictionary is similar to a list, but you access values by looking up a key instead of an index. A key can be any string or number. Dictionaries are enclosed in curly braces, like so:
 # d = {'key1' : 1, 'key2' : 2, 'key3' : 3}
 
+# Assigning a dictionary with three key-value pairs to residents:
+residents = {'Puffin' : 104, 'Sloth' : 105, 'Burmese Python' : 106}
+
+print residents['Puffin'] # Prints Puffin's room number
+
+# Your code here!
+print residents['Sloth'] 
+print residents['Burmese Python']
