69aec4f50ee9e91704d3633d59acd62e9e257042
Dale Stephenson <dalestephenson@zohomail.com>
2022-02-13 20:04:13 +1300

    Lists

--- lists.py
+++ lists.py
@@ -73,16 +73,16 @@ animals = "catdogfrog"
 
 # The first three characters of animals
 cat = animals[:3]
-
 print cat
 
 # The fourth through sixth characters
 dog = animals[3:6]
-
 print dog
 
 # From the seventh character to the end
 frog = animals[6:]
-
 print frog
+
+not_pets = ["ant", "bat", "cat"]
+print not_pets.index("bat")
 
