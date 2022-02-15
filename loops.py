6d852557bbf1d33b324ff011f6caf5b7e194c6f1
Dale Stephenson <dalestephenson@zohomail.com>
2022-02-15 20:06:38 +1300

    Loops

--- loops.py
+++ loops.py
@@ -246,3 +246,27 @@ print "Grand Total " + str(total)
 
 #--------------------------------
 
+shopping_list = ["banana", "orange", "apple"]
+
+stock = {
+  "banana": 6,
+  "apple": 0,
+  "orange": 32,
+  "pear": 15
+}
+    
+prices = {
+  "banana": 4,
+  "apple": 2,
+  "orange": 1.5,
+  "pear": 3
+}
+
+# Write your code below!
+def compute_bill(food):
+  total = 0
+  for item in food:
+    total = total + prices[item]
+  return total
+
+print compute_bill(shopping_list)
