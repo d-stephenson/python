e34264f72f89fa7cdd2b44c6205b506f28e00fe3
Dale Stephenson <dalestephenson@zohomail.com>
2022-02-17 12:00:57 +1300

    list_functions

--- lists_function.py
+++ lists_function.py
@@ -152,6 +152,21 @@ def total(numbers):
     
 print total(n)
 
+n = ["Michael", "Lieberman"]
+
+def join_strings(words):
+  result = ""
+  for word in words:
+    result += word
+  return result
+
+print join_strings(n)
+
+
+
+
+
+
 
 
 
