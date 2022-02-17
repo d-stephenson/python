190b129566ecfb3e172db8468abed8cfe9cb490f
Dale Stephenson <dalestephenson@zohomail.com>
2022-02-17 21:24:21 +1300

    game

--- calc.py
+++ calc.py
@@ -6,6 +6,6 @@ def calc(salary, hours):
     amount = int(salary) * hours * 52
     for i in [int(amount * 0.58), int(amount * 1.18), int(amount * 0.79)]:
         comparison.append(i)
-    return "CAD " + str(amount) + " | GBP " + str(comparison[0]) + " | NZD " + str(comparison[1]) + " | USD " + str(comparison[0])
+    return "CAD " + str(amount) + " | GBP " + str(comparison[0]) + " | NZD " + str(comparison[1]) + " | USD " + str(comparison[2])
 
 print calc(56.23, 35)
