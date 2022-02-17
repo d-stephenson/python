6e4b8272d1d06bc5fdf728b5e6dcb89a4de29e1f
Dale Stephenson <dalestephenson@zohomail.com>
2022-02-17 20:12:32 +1300

    game

---
+++ calc.py
@@ -0,0 +1,10 @@
+# Salary calc 
+
+def calc(salary):
+    amount = 0
+    comparison = 0
+    amount = salary * 35 * 52
+    comparison = amount * 0.58
+    return str(amount) + " GDP " + str(comparison)
+
+print calc(58)
