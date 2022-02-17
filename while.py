1c1b5383af415b4c3966507afe9ac2ff521733a9
Dale Stephenson <dalestephenson@zohomail.com>
2022-02-18 12:46:05 +1300

    while

---
+++ while.py
@@ -0,0 +1,19 @@
+# While
+
+# The while loop is similar to an if statement: it executes the code 
+# inside of it if some condition is true. The difference is that the 
+# while loop will continue to execute as long as the condition is true
+# In other words, instead of executing if something is true, it executes 
+# while that thing is true
+
+count = 0
+
+if count < 5:
+  print "Hello, I am an if statement and count is", count
+
+while count < 5:
+  print "Hello, I am a while and count is", count
+  count += 1
+  
+# The condition is the expression that decides whether the loop is going 
+# to continue being executed or not. There are 5 steps to this program
