00adfdbdf55c7eda49125a243a631c0acf7364ea
Dale Stephenson <dalestephenson@zohomail.com>
2022-02-18 12:37:41 +1300

    game

--- game.py
+++ game.py
@@ -41,10 +41,15 @@ for turn in range(4):
   guess_row = int(raw_input("Guess Row: "))
   guess_col = int(raw_input("Guess Col: "))
 
-  if guess_row == ship_row and guess_col == ship_col:
+  if guess_row == ship_row and \
+     guess_col == ship_col:
     print "Congratulations! You sunk my battleship!"
+    break
   else:
-    if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+    if (guess_row < 0 or \
+        guess_row > 4) or \
+       (guess_col < 0 or \
+        guess_col > 4):
       print "Oops, that's not even in the ocean."
     elif(board[guess_row][guess_col] == "X"):
       print "You guessed that one already."
