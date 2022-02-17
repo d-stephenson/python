11693fbfc200849b26ce2b2442fdd405df444ccf
Dale Stephenson <dalestephenson@zohomail.com>
2022-02-18 12:08:37 +1300

    game

--- game.py
+++ game.py
@@ -25,3 +25,11 @@ guess_col = int(raw_input("Guess Col: "))
 
 if guess_row == ship_row and guess_col == ship_col:
   print "Congratulations! You sank my battleship!"
+else:
+  if guess_row not in range(5) or \
+     guess_col not in range(5):
+    print "Oops, that's not even in the ocean."
+  else:
+    print "You missed my battleship!"
+    board[guess_row][guess_col] = "X"
+    print_board(board)
