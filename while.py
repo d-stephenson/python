548bd974fb2f2f0abb5134cc30b2c9ce2c6a9cf6
Dale Stephenson <dalestephenson@zohomail.com>
2022-02-18 14:40:17 +1300

    while

--- while.py
+++ while.py
@@ -17,3 +17,23 @@ while count < 5:
   
 # The condition is the expression that decides whether the loop is going 
 # to continue being executed or not. There are 5 steps to this program
+
+loop_condition = True
+
+while loop_condition:
+  print "I am a loop"
+  loop_condition = False
+  
+num = 1
+
+while num < 11:  # Fill in the condition
+  # Print num squared
+  # Increment num (make sure to do this!)
+  print num ** 2
+  num +=1
+  
+choice = raw_input('Enjoying the course? (y/n)')
+
+while choice != "y" and choice != "no": 
+  choice = raw_input("Sorry, I didn't catch that. Enter again: ")
+
