e8e2bbcf0efef639d5c9076d3e3871a38eb9e056
Dale Stephenson <dalestephenson@zohomail.com>
2022-02-20 20:12:10 +1300

    Problems

--- problems.py
+++ problems.py
@@ -7,3 +7,13 @@ def is_even(x):
     return False
 
 print is_even(26572)
+
+#--------------------------------
+
+def is_int(x):
+  absolute = abs(x)
+  rounded = round(absolute)
+  return absolute - rounded == 0
+
+print is_int(10)
+print is_int(10.5)
