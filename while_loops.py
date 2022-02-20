26c5c1b121ead79e6035476ad11b91d32db14d1c
Dale Stephenson <dalestephenson@zohomail.com>
2022-02-20 19:37:11 +1300

    Loops

--- while_loops.py
+++ while_loops.py
@@ -163,3 +163,18 @@ choices = ['pizza', 'pasta', 'salad', 'nachos']
 print 'Your choices are:'
 for index, item in enumerate(choices):
   print index + 1, item
+  
+#--------------------------------
+
+# Multiple lists
+
+# zip will create pairs of elements when passed two lists, and will stop at the end of the shorter list
+
+list_a = [3, 9, 17, 15, 19]
+list_b = [2, 4, 8, 10, 30, 40, 50, 60, 70, 80, 90]
+
+for a, b in zip(list_a, list_b):
+ if a > b:
+   print a
+ else:
+   print b 
