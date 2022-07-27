cc5fc18ea0ed94c2a2790341bffd7e9d2fa99c6a
Dale Stephenson <dalestephenson@zohomail.com>
2022-07-27 20:19:30 +1200

    cheatsheet

---
+++ cheatsheet.py
@@ -0,0 +1,11 @@
+Comments
+A comment is a piece of text within a program that is not executed. It can be used to provide additional information to aid in understanding the code.
+
+The # character is used to start a comment and it continues until the end of the line.
+
+# Comment on a single line
+ 
+user = "JDoe" # Comment after code
+
+# ////////////////////////////////////////////
+
