8cd8866f4736ab40bae0010c4bf9187844123933
Dale Stephenson <dalestephenson@zohomail.com>
2022-07-27 21:15:39 +1200

    cheatsheet

--- testing.py
+++ testing.py
@@ -1,6 +1,6 @@
 # Cheatsheet testing
 
-        # Arithmetic Operations
+# Arithmetic Operations
 
 result = ((1000 + 50 - 750) * 4) / 2
 print(result)
@@ -31,3 +31,23 @@ message = "Part 1 of message "
 print(message)
 message += " with part 2 of message"
 print(message)
+
+# Variables
+
+points = 100
+print(points)
+
+    # Variables can be changed
+points = 1875.35
+print(points)
+
+def users(username_first, username_last, user_id):
+    if user_id > 500:
+        return username_first + username_last
+    
+print users('Dale', ' Stephenson', 887016356)
+
+
+    
+    
+
