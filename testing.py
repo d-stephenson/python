273f7e9e42d9538b58efe0cda1b2a76255b90227
Dale Stephenson <dalestephenson@zohomail.com>
2022-07-27 20:49:48 +1200

    cheatsheet

--- testing.py
+++ testing.py
@@ -5,3 +5,15 @@
 result = ((1000 + 50 - 750) * 4) / 2
 print(result)
 
+print(31 / 4)
+print(31.00 / 4.00)
+
+# % for modulus (returns the remainder)
+
+print(31 % 4)
+print(155 % 6)
+
+# ** for exponentiation
+
+print(5 ** 3)
+print(10 ** 3)
