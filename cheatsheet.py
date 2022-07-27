5f36359ca2ec7fbf8b126dff5ff87bf8fabce9d0
Dale Stephenson <dalestephenson@zohomail.com>
2022-07-27 20:19:57 +1200

    cheatsheet

--- cheatsheet.py
+++ cheatsheet.py
@@ -9,3 +9,24 @@ user = "JDoe" # Comment after code
 
 # ////////////////////////////////////////////
 
+Arithmetic Operations
+Python supports different types of arithmetic operations that can be performed on literal numbers, variables, or some combination. The primary arithmetic operators are:
+
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+% for modulus (returns the remainder)
+** for exponentiation
+
+# Arithmetic operations
+ 
+result = 10 + 30
+result = 40 - 10
+result = 50 * 5
+result = 16 / 4
+result = 25 % 2
+result = 5 ** 3
+
+# ////////////////////////////////////////////
+
